3b82cf046c263e5082b8f87f0b17d99318af1ebd
Santosh Molleti <m.santoshkumar@ocadoretail.com>
2024-07-26 13:13:38 +0530

    shortend way of reusable components

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,10 +3,12 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 const name = "Santosh";
-function Logo(props) {
+
+const data = [{ link: "https://vitejs.dev", srcName: viteLogo, classes: "", altName: "Vite" }, { link: "https://react.dev", srcName: reactLogo, classes: "react", altName: "React" }];
+function Logo({ link, srcName, classes, altName }) {
   return (<><div>
-    <a href={props.link} target="_blank">
-      <img src={props.srcName} className={props.class + " logo"} alt={props.altName + " logo"} />
+    <a href={link} target="_blank">
+      <img src={srcName} className={classes + " logo"} alt={altName + " logo"} />
     </a>
   </div>
   </>);
@@ -15,8 +17,8 @@ function Logo(props) {
 function App() {
   return (
     <>
-      <Logo link="https://vitejs.dev" srcName={viteLogo} class="" altName="Vite" />
-      <Logo link="https://react.dev" srcName={reactLogo} class="react" altName="React" />
+      <Logo {...data[0]} />
+      <Logo {...data[1]} />
       <h1>Vite + React</h1>
       <h1>{name}</h1>
     </>
